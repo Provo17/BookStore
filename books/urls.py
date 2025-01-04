5a0d56a3044e8bd601417853f726e391f69e227f
Ray Butler <ray@Rays-MacBook-Pro.local>
2025-01-03 21:47:13 -0600

    changed book model views

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import book_detail
+from .views import book_detail, book_list
 
 urlpatterns = [
+    path('', book_list, name='book_list'),
     path('book/<int:pk>/', book_detail, name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Book  # Ensure 'Book' is the correct model name
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookstore/book_list.html', {'books': books})
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'bookstore/book_detail.html', {'book': book})
