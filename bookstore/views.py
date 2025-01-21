2d7676ad0ba038bef0045b83b3f4bf0d6f1d52c6
Leander Butler <lsutigers308@gmail.com>
2025-01-21 00:13:21 -0600

    Created Best sellers slider on homepage

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from books.models import Book
 def index(request):
-    books = Book.objects.filter(rating=5).order_by('title')
+    books = Book.objects.filter(rating=5).order_by('title')[:8]
     return render(request, 'bookstore/homepage.html', {'books': books})  # Include the 'bookstore/' prefix
